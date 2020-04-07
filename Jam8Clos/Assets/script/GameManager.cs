d66e1251d6dd5e6b48d2d428f74b06e1125e7040
sangshu <54836318+sangshu-sang@users.noreply.github.com>
2020-04-07 12:33:29 +0200

    Proto déplacement+Caméra

--- Jam8Clos/Assets/script/GameManager.cs
+++ Jam8Clos/Assets/script/GameManager.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [Space]
     [Header("GD Friendly")]
     [Space]
 
+    public float speed;
+    
+   
+    [Space]
     public float TimerFaisceau;
+    [Space]
+    public float SensitivityH;
+    public float SensitivityV;
+    public float OffsetX;
+    public float OffsetY;
+    public float OffsetZ;
+
 
     [Space]
-    [Header("Public Manager ")]
     [Space]
-
-    public static GameManager instance;
+    [Header("Public Manager")]
+    public GameObject camera;
+    public GameObject Player;
     public bool FaisceauOK = false;
     public bool PressionOK = false;
     int currentPress;
     public  int LastPress;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    public static GameManager instance;
+    
 
     void Awake()
     {
@@ -36,6 +50,19 @@ public class GameManager : MonoBehaviour
         
     }
 
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+
+        yaw += SensitivityH * Input.GetAxis("Mouse X");
+        pitch -= SensitivityV * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -90, 15);
+        camera.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        Player.transform.eulerAngles = new Vector3(Player.transform.eulerAngles.x, yaw, Player.transform.eulerAngles.z);
+        camera.transform.position = new Vector3(Player.transform.position.x+OffsetX, Player.transform.position.y+OffsetY, Player.transform.position.z+OffsetZ);
+    }
 
     public void VerifPression(int ordre)
     {
--- Jam8Clos/Assets/script/PlayerController.cs
+++ Jam8Clos/Assets/script/PlayerController.cs
@@ -14,23 +14,24 @@ public class PlayerController : MonoBehaviour
  
 
 
-    public float speed;
-    public float maxSpeed;
-    public float frottement;
+     float speed;
+     
 
     // Start is called before the first frame update
     void Start()
     {
+        speed = GameManager.instance.speed;
+        
+     
+
         rb = GetComponent<Rigidbody>();
 
     }
 
     private void FixedUpdate()
     {
-
-
-
-        //Move Velo
+        
+        /*
         if (Avant)
         {
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z + speed * Time.fixedTime);
@@ -41,64 +42,106 @@ public class PlayerController : MonoBehaviour
         }
         if (Droite)
         {
-            rb.velocity = new Vector3(rb.velocity.x + speed * Time.fixedTime, rb.velocity.y, rb.velocity.z);
+            transform.Rotate(transform.rotation.x, transform.rotation.y+ Rotaspeed*Time.deltaTime, transform.rotation.z,Space.World);
         }
         if (Gauche)
         {
-            rb.velocity = new Vector3(rb.velocity.x + speed * Time.fixedTime * -1, rb.velocity.y, rb.velocity.z);
-        }
+            transform.Rotate(transform.rotation.x, transform.rotation.y - Rotaspeed*Time.deltaTime, transform.rotation.z, Space.World);
+        }*/
 
-        //Maxspeed
-        if (rb.velocity.x > maxSpeed)
-        {
-            rb.velocity = new Vector3(maxSpeed, rb.velocity.y, rb.velocity.z);
-        }
-        if (rb.velocity.x < -maxSpeed)
-        {
-            rb.velocity = new Vector3(-maxSpeed, rb.velocity.y, rb.velocity.z);
-        }
-        if (rb.velocity.z > maxSpeed)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxSpeed);
-        }
-        if (rb.velocity.z < -maxSpeed)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -maxSpeed);
-        }
 
-        //Frottement
-        if (!Gauche && !Droite)
-        {
-            if (rb.velocity.x < -0.5f)
-            {
-                rb.velocity = new Vector3(rb.velocity.x + frottement * Time.deltaTime, rb.velocity.y, rb.velocity.z);
-            }
-            if (rb.velocity.x > 0.5f)
-            {
-                rb.velocity = new Vector3(rb.velocity.x - frottement * Time.deltaTime, rb.velocity.y, rb.velocity.z);
-            }
-            if (rb.velocity.x > -0.5f && rb.velocity.x < 0.5f)
-            {
-                rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
-            }
+          //Move Velocity
+          if (Avant)
+          {
+            transform.position += transform.forward * Time.deltaTime * speed;
+           
+          }
+          if (Reculer)
+          {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+         
+          }
+          if (Droite)
+          {
+            transform.position += transform.right * Time.deltaTime * speed;
+           
+          }
+          if (Gauche)
+          {
+              
+            transform.position -= transform.right * Time.deltaTime * speed;
+          }
 
-        }
-        if (!Avant && !Reculer)
-        {
-            if (rb.velocity.z < -0.5f)
-            {
-                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z + frottement * Time.deltaTime);
-            }
-            if (rb.velocity.z > 0.5f)
-            {
-                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z - frottement * Time.deltaTime);
-            }
-            if (rb.velocity.z > -0.5f && rb.velocity.z < 0.5f)
-            {
-                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
-            }
+         /* if (Avant)
+          {
 
-        }
+           rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z + speed * Time.fixedTime);
+          }
+          if (Reculer)
+          {
+              rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z + speed * Time.fixedTime * -1);
+          }
+          if (Droite)
+          {
+           
+            rb.velocity = new Vector3(rb.velocity.x + speed * Time.fixedTime, rb.velocity.y, rb.velocity.z);
+          }
+          if (Gauche)
+          {
+              rb.velocity = new Vector3(rb.velocity.x + speed * Time.fixedTime * -1, rb.velocity.y, rb.velocity.z);
+          }
+
+          //Maxspeed
+          if (rb.velocity.x > maxSpeed)
+          {
+              rb.velocity = new Vector3(maxSpeed, rb.velocity.y, rb.velocity.z);
+          }
+          if (rb.velocity.x < -maxSpeed)
+          {
+              rb.velocity = new Vector3(-maxSpeed, rb.velocity.y, rb.velocity.z);
+          }
+          if (rb.velocity.z > maxSpeed)
+          {
+              rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxSpeed);
+          }
+          if (rb.velocity.z < -maxSpeed)
+          {
+              rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -maxSpeed);
+          }
+
+          //Frottement
+          if (!Gauche && !Droite)
+          {
+              if (rb.velocity.x < -0.5f)
+              {
+                  rb.velocity = new Vector3(rb.velocity.x + frottement * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+              }
+              if (rb.velocity.x > 0.5f)
+              {
+                  rb.velocity = new Vector3(rb.velocity.x - frottement * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+              }
+              if (rb.velocity.x > -0.5f && rb.velocity.x < 0.5f)
+              {
+                  rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+              }
+
+          }
+          if (!Avant && !Reculer)
+          {
+              if (rb.velocity.z < -0.5f)
+              {
+                  rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z + frottement * Time.deltaTime);
+              }
+              if (rb.velocity.z > 0.5f)
+              {
+                  rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, rb.velocity.z - frottement * Time.deltaTime);
+              }
+              if (rb.velocity.z > -0.5f && rb.velocity.z < 0.5f)
+              {
+                  rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
+              }
+
+          }*/
 
 
 
