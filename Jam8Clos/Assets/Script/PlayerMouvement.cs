0bae2205f3c0177d7077273e7bb93addfba72c98
Leo Nimsgern <nimsgernleo@gmail.com>
2020-04-08 12:06:33 +0200

    enigme 3 non finaliser

--- Jam8Clos/Assets/Script/PlayerMouvement.cs
+++ Jam8Clos/Assets/Script/PlayerMouvement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMouvement : MonoBehaviour
 {
+
+    public CharacterController controller;
+
+    public float speed = 12f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class PlayerMouvement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed *Time.deltaTime);
     }
 }
---
+++ Jam8Clos/Assets/Script/RayonEnigme.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayonEnigme : MonoBehaviour
+{
+    public Material c;
+    public List<GameObject> Light;
+    public GameObject[] Lettre;
+    public GameObject Verrou;
+    public int cpt = 0;
+
+    private bool Lettre_1 = false;
+    private bool Lettre_2 = false;
+    private bool Lettre_3 = false;
+    private bool Comfirm_Lettre_1 = false;
+    private bool Comfirm_Lettre_2 = false;
+    private bool Comfirm_Lettre_3 = false;
+
+    private void Start()
+    {
+        Lettre_1 = false;
+        Lettre_2 = false;
+        Lettre_3 = false;
+    }
+    void Update()
+    {
+        if (cpt == 3)
+        {
+            if (Lettre_1 & Lettre_2 & Lettre_3)
+            {
+                Verrou.SetActive(true);
+            }
+            else
+            {
+                Lettre_1 = false;
+                Lettre_2 = false;
+                Lettre_3 = false;
+                Comfirm_Lettre_1 = false;
+                Comfirm_Lettre_2 = false;
+                Comfirm_Lettre_3 = false;
+                cpt = 0;
+                Light.Clear();
+            }
+            
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (cpt < 3)
+        {
+            if(other.gameObject == Lettre[0] && !Comfirm_Lettre_1)
+            {
+                Check();
+                Lettre_1 = true;
+                Comfirm_Lettre_1 = true;
+                cpt++;
+            }
+            else if (other.gameObject == Lettre[1] && !Comfirm_Lettre_2)
+            {
+                Lettre_2 = true;
+                Comfirm_Lettre_2 = true;
+                cpt++;
+            }
+            else if ( other.gameObject == Lettre[2] && !Comfirm_Lettre_3)
+            {
+                Lettre_3 = true;
+                Comfirm_Lettre_3 = true;
+                cpt++;
+            }
+            else
+            {
+                if(other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1)
+                {
+                    Lettre_1 = false;
+                    Comfirm_Lettre_1 = true;
+                    cpt++;
+                }
+                else if(other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1 && !Comfirm_Lettre_2)
+                {
+                    Lettre_2 = false;
+                    Comfirm_Lettre_2 = true;
+                    cpt++;
+                }
+                else if (other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1 && !Comfirm_Lettre_3)
+                {
+                    Lettre_3 = false;
+                    Comfirm_Lettre_3 = true;
+                    cpt++;
+                }
+                
+            }
+        }
+    }
+
+    IEnumerator Check()
+    {
+
+        yield return new WaitForSeconds(2f);
+
+    }
+}
