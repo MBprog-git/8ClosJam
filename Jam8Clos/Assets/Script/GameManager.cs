c25da69a1e9af992ca97e105cc7850afca91eb6a
Leo Nimsgern <nimsgernleo@gmail.com>
2020-04-08 15:19:27 +0200

    modif

---
+++ Jam8Clos/Assets/Script/GameManager.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("GD Friendly")]
+    [Space]
+
+    public float speed;
+    
+   
+    [Space]
+    public float TimerFaisceau;
+    public float TimerLettre;
+    public float TimerJourNuit;
+    [Space]
+    public float SensitivityH;
+    public float SensitivityV;
+    public float OffsetX;
+    public float OffsetY;
+    public float OffsetZ;
+
+
+    [Space]
+    [Space]
+    [Header("Public Manager")]
+    public GameObject Cam;
+    public GameObject Player;
+    public GameObject Cubetest;
+    public GameObject SphereTest;
+    public GameObject faisceau;
+    public GameObject Diode1;
+    public GameObject Diode2;
+    public GameObject Diode3;
+    public Image Paupiere;
+    public Light Lumiere;
+    public bool Jour = true;
+    public bool FaisceauOK = false;
+    public bool PressionOK = false;
+    public bool LettreOK = false;
+    public  int LastPress;
+    int currentPress;
+    float ChronoCycle;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    public static GameManager instance;
+
+    private List<CycleItem> _cycleItems = new List<CycleItem>();
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        foreach(CycleItem CI in Resources.FindObjectsOfTypeAll(typeof(CycleItem)))
+        {
+            _cycleItems.Add(CI);
+        }
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChronoCycle = TimerJourNuit;
+        //CycleDay();
+        Diode1.GetComponent<Renderer>().material.color = Color.red;
+        Diode2.GetComponent<Renderer>().material.color = Color.red;
+        Diode3.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        //Camera
+        if (Input.GetMouseButtonDown(0)) { 
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+        yaw += SensitivityH * Input.GetAxis("Mouse X");
+        pitch -= SensitivityV * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -90, 15);
+        Cam.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        Player.transform.eulerAngles = new Vector3(Player.transform.eulerAngles.x, yaw, Player.transform.eulerAngles.z);
+        Cam.transform.position = new Vector3(Player.transform.position.x+OffsetX, Player.transform.position.y+OffsetY, Player.transform.position.z+OffsetZ);
+
+        //Jour-Nuit
+        if (ChronoCycle<0)
+        {
+            CycleDay();
+        }
+        else
+        {
+            ChronoCycle -= Time.deltaTime;
+        }
+        //clignement
+        if (Paupiere.color.a > 0)
+        {
+            Paupiere.color = new Color(Paupiere.color.r,Paupiere.color.g,Paupiere.color.b,Paupiere.color.a - Time.deltaTime);
+            
+        }
+        //Porte
+        if(FaisceauOK && PressionOK && LettreOK)
+        {
+            //Porte Ouverte
+        }
+    }
+
+    public void CycleDay()
+    {
+        Jour = !Jour;
+
+        if (Jour)
+        {
+           /* SphereTest.SetActive(true);
+            Cubetest.GetComponent<Animation>().Stop();*/
+
+            foreach(CycleItem CI in _cycleItems)
+            {
+                CI.Cycle();
+            }
+            if (!FaisceauOK) { 
+            faisceau.SetActive(true);
+            }
+            
+            Lumiere.intensity = 4;
+            
+        }
+        else
+        {
+            /* Cubetest.GetComponent<Animation>().Play(Cubetest.GetComponent<Animation>().clip.name);
+             SphereTest.SetActive(false);*/
+            foreach (CycleItem CI in _cycleItems)
+            {
+                CI.Cycle();
+            }
+            faisceau.SetActive(false);
+            Lumiere.intensity = 1;
+        }
+        Paupiere.color = new Color(Paupiere.color.r, Paupiere.color.g, Paupiere.color.b, 1);
+        ChronoCycle = TimerJourNuit;
+    }
+    public void VerifPression(int ordre)
+    {
+        if (ordre == currentPress + 1)
+        {
+            currentPress = ordre;
+            if(currentPress == LastPress)
+            {
+                PressionOK = true;
+                Diode2.GetComponent<Renderer>().material.color = Color.green;
+
+            }
+        }
+        else
+        {
+            currentPress = 0;
+        }
+    }
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+    public void MyLoadScene(string nameScene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nameScene);
+     
+    }
+}
--- Jam8Clos/Assets/Script/RayonEnigme.cs
+++ Jam8Clos/Assets/Script/RayonEnigme.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class RayonEnigme : MonoBehaviour
 {
-    public Material c;
+    float timer;
+    float Temps;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Lettre")
+        {
+            timer = Temps;
+        }
+    }
+    
+    
     public List<GameObject> Light;
     public GameObject[] Lettre;
     public GameObject Verrou;
     public int cpt = 0;
 
-    private bool Lettre_1 = false;
-    private bool Lettre_2 = false;
-    private bool Lettre_3 = false;
+    public bool Lettre_1 = false;
+    public bool Lettre_2 = false;
+    public bool Lettre_3 = false;
     private bool Comfirm_Lettre_1 = false;
     private bool Comfirm_Lettre_2 = false;
     private bool Comfirm_Lettre_3 = false;
 
     private void Start()
     {
+        Temps = GameManager.instance.TimerLettre;
         Lettre_1 = false;
         Lettre_2 = false;
         Lettre_3 = false;
@@ -29,7 +42,9 @@ public class RayonEnigme : MonoBehaviour
         {
             if (Lettre_1 & Lettre_2 & Lettre_3)
             {
-                Verrou.SetActive(true);
+                GameManager.instance.LettreOK = true;
+                GameManager.instance.Diode3.GetComponent<Renderer>().material.color = Color.green;
+                GameManager.instance.Diode3.SetActive(false);
             }
             else
             {
@@ -47,58 +62,77 @@ public class RayonEnigme : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (cpt < 3)
+        if (other.gameObject.tag == "Lettre")
         {
-            if(other.gameObject == Lettre[0] && !Comfirm_Lettre_1)
+            
+            timer -= Time.deltaTime;
+
+            if (timer < 0)
             {
-                Check();
-                Lettre_1 = true;
-                Comfirm_Lettre_1 = true;
-                cpt++;
-            }
-            else if (other.gameObject == Lettre[1] && !Comfirm_Lettre_2)
-            {
-                Lettre_2 = true;
-                Comfirm_Lettre_2 = true;
-                cpt++;
-            }
-            else if ( other.gameObject == Lettre[2] && !Comfirm_Lettre_3)
-            {
-                Lettre_3 = true;
-                Comfirm_Lettre_3 = true;
-                cpt++;
-            }
-            else
-            {
-                if(other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1)
+                Debug.Log("fin timer");
+
+                if (cpt < 3)
                 {
-                    Lettre_1 = false;
-                    Comfirm_Lettre_1 = true;
-                    cpt++;
+
+                    Debug.Log("choix Lettre");
+                    if (other.gameObject == Lettre[0] || other.gameObject == Lettre[1] || other.gameObject == Lettre[2] && !Comfirm_Lettre_1)
+                    {
+                        Debug.Log("Lettre1");
+                        Lettre_1 = true;
+                        Comfirm_Lettre_1 = true;
+                        cpt++;
+                    }
+                    else if (other.gameObject == Lettre[0] || other.gameObject == Lettre[1] || other.gameObject == Lettre[2] && !Comfirm_Lettre_2)
+                    {
+                        Debug.Log("Lettre2");
+                        Lettre_2 = true;
+                        Comfirm_Lettre_2 = true;
+                        cpt++;
+                    }
+                    else if (other.gameObject == Lettre[0] || other.gameObject == Lettre[1] || other.gameObject == Lettre[2] && !Comfirm_Lettre_3)
+                    {
+                        Debug.Log("Lettre3");
+                        Lettre_3 = true;
+                        Comfirm_Lettre_3 = true;
+                        cpt++;
+                    }
+                    else
+                    {
+                        if (other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1)
+                        {
+                            Debug.Log("Lettreautre");
+                            Lettre_1 = false;
+                            Comfirm_Lettre_1 = true;
+                            cpt++;
+                        }
+                        else if (other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1 && !Comfirm_Lettre_2)
+                        {
+                            Debug.Log("Lettreautre");
+                            Lettre_2 = false;
+                            Comfirm_Lettre_2 = true;
+                            cpt++;
+                        }
+                        else if (other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1 && !Comfirm_Lettre_3)
+                        {
+                            Debug.Log("Lettreautre");
+                            Lettre_3 = false;
+                            Comfirm_Lettre_3 = true;
+                            cpt++;
+                        }
+
+                    }
                 }
-                else if(other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1 && !Comfirm_Lettre_2)
-                {
-                    Lettre_2 = false;
-                    Comfirm_Lettre_2 = true;
-                    cpt++;
-                }
-                else if (other.gameObject == Lettre[3] || other.gameObject == Lettre[4] || other.gameObject == Lettre[5] || other.gameObject == Lettre[6] || other.gameObject == Lettre[7] && !Comfirm_Lettre_1 && !Comfirm_Lettre_3)
-                {
-                    Lettre_3 = false;
-                    Comfirm_Lettre_3 = true;
-                    cpt++;
-                }
-                
+               
+
+
             }
         }
-    }
-
-    IEnumerator Check()
-    {
-
-        yield return new WaitForSeconds(2f);
-
+        else
+        {
+            timer = Temps;
+        }
+        
     }
 }
