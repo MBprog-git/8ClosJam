01a1b72b84ce488f6f6adea0c38bfd0d3ad67c2b
sangshu <54836318+sangshu-sang@users.noreply.github.com>
2020-04-09 11:01:22 +0200

    Retag

---
+++ Jam8Clos/Assets/Script/SfxManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+
+    //public List<Audiocl>
+    AudioSource AS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
